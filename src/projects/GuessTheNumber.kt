7b69ed2746297fd65efe95c2d9e0fe262b34c854
volostnykh-aa <volostnykh-aa@zvuk.com>
2025-03-02 03:54:05 +0400

    menu

--- src/projects/GuessTheNumber.kt
+++ src/projects/GuessTheNumber.kt
@@ -2,7 +2,10 @@ package projects
 
 fun main(){
 
-    println("1. Запустить игру")
     println("2. Гид по игре")
     println("3. Выйти из игры")
+}
+
+fun playGame() {
+    println("1. Запустить игру")
 }
